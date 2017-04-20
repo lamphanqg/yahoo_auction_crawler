b4199639378060c022dcb735df6eabd3aa4d9b2a
Phan Quang Lam <lam.phan.qg@gmail.com>
2017-04-20 12:49:18 +0900

    remove logging

--- yahoo_auc_crawler/spiders/products_spider.py
+++ yahoo_auc_crawler/spiders/products_spider.py
@@ -5,14 +5,9 @@ class ProductsSpider(scrapy.Spider):
   name = "products"
 
   start_urls = ['https://auctions.yahoo.co.jp/seller/tolvio0256?ngram=1']
-  link_number = 0
-  product_number = 0
 
   def parse(self, response):
     for href in response.css('.a1wrp>h3>a::attr(href)').extract():
-      self.link_number += 1
-      self.logger.info('NUMBER OF LINKS: ' + str(self.link_number))
-      self.logger.info('LINK: ' + response.urljoin(href))
       yield scrapy.Request(response.urljoin(href), callback = self.parse_product)
 
     next_page = response.css('.next>a::attr(href)').extract_first()
@@ -24,8 +19,6 @@ class ProductsSpider(scrapy.Spider):
     def extract_with_css(query):
       return response.css(query).extract_first().strip()
 
-    self.product_number += 1
-    self.logger.info('NUMBER OF PRODUCTS: ' + str(self.product_number))
     yield {
       'title': extract_with_css('h1.ProductTitle__text::text')
     }
