3cc1ff0d0bb810837a65821eb583af0c77347c35
Unknown <lam.phan.qg@gmail.com>
2017-04-24 01:00:20 +0900

    Add full csv format

--- yahoo_auc_crawler/spiders/products_spider.py
+++ yahoo_auc_crawler/spiders/products_spider.py
@@ -1,24 +1,101 @@
+from urllib.parse import urlparse
+from urllib.parse import parse_qsl
 import scrapy
-from scrapy import signals
+import requests
 
 class ProductsSpider(scrapy.Spider):
-  name = "products"
+    name = "products"
 
-  start_urls = ['https://auctions.yahoo.co.jp/seller/tolvio0256?ngram=1']
+    start_urls = ['https://auctions.yahoo.co.jp/seller/tolvio0256?ngram=1']
+    product_num = 0
 
-  def parse(self, response):
-    for href in response.css('.a1wrp>h3>a::attr(href)').extract():
-      yield scrapy.Request(response.urljoin(href), callback = self.parse_product)
+    def parse(self, response):
+        for href in response.css('.a1wrp>h3>a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(href), callback=self.parse_product)
 
-    next_page = response.css('.next>a::attr(href)').extract_first()
-    if next_page is not None:
-      next_page = response.urljoin(next_page)
-      yield scrapy.Request(next_page, callback = self.parse)
+        next_page = response.css('.next>a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
 
-  def parse_product(self, response):
-    def extract_with_css(query):
-      return response.css(query).extract_first().strip()
+    def parse_product(self, response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
 
-    yield {
-      'title': extract_with_css('h1.ProductTitle__text::text')
-    }
+        self.product_num += 1
+        strnumber = str(self.product_num).zfill(4)
+
+        category_link = response.css('.acMdNaviBox .st04>a::attr(href)').extract_first()
+        query_string = urlparse(category_link).query
+        category_str = parse_qsl(query_string)[0][1]
+
+        init_price_str = response.css('.ProductDetail__body .l-right .ProductDetail__description::text').extract()[3].strip()
+        init_price = int(init_price_str.replace(' 円', '').replace(',', ''))
+
+        images = ['', '', '']
+        image_urls = response.css('.ProductImage__inner>img::attr(src)').extract()
+        if image_urls:
+            for index, image_url in enumerate(image_urls):
+                file_name = strnumber + '_' + str(index + 1) + '.jpg'
+                file_path = 'images/' + file_name
+                with open(file_path, 'wb') as handle:
+                    img_response = requests.get(image_url, stream=True)
+                    if not img_response.ok:
+                        self.logger.error(img_response)
+                    images[index] = file_name
+                    for block in img_response.iter_content(1024):
+                        if not block:
+                            break
+                        handle.write(block)
+
+        yield {
+            '#': strnumber,
+            'カテゴリ': category_str,
+            'タイトル': extract_with_css('h1.ProductTitle__text::text'),
+            '説明': '',
+            '開始価格': init_price,
+            '即決価格': '',
+            '個数': int(response.css('.ProductDetail__body .l-left .ProductDetail__description::text').extract()[1].strip()),
+            '開催期間': '',
+            '終了時間': '',
+            '画像1': images[0],
+            '画像２': images[1],
+            '画像３': images[2],
+            '商品発送元の都道府県': '東京都',
+            '送料負担': '落札者',
+            '代金支払い': '先払い',
+            'Yahoo!かんたん決済': 'はい',
+            '銀行振込': 'はい',
+            'かんたん取引': 'はい',
+            '銀行ID1': '3197870',
+            '銀行名1': 'ゆうちょ',
+            '現金書留': 'いいえ',
+            '商品代引': 'いいえ',
+            '商品の状態': response.css('.ProductDetail__body .l-left .ProductDetail__description::text').extract()[0].strip(),
+            '返品の可否': '返品可',
+            '入札者評価制限': 'はい',
+            '悪い評価の割合での制限': 'はい',
+            '入札者認証制限': 'はい',
+            '自動延長': 'はい',
+            '早期終了': 'はい',
+            '値下げ交渉': 'いいえ',
+            '自動再出品': '3',
+            '自動値下げ': 'いいえ',
+            '自動値下げ価格変更率': '',
+            '太字テキスト': 'いいえ',
+            '背景色': 'いいえ',
+            '贈答品アイコン': 'いいえ',
+            '送料固定': 'はい',
+            'ネコポス': 'いいえ',
+            'ネコ宅急便コンパクト': 'いいえ',
+            'ネコ宅急便': 'いいえ',
+            'はこBOON': 'いいえ',
+            'はこBOONmini': 'いいえ',
+            '配送方法1': '当店契約発送代行業者',
+            '配送方法1全国一律価格': 800,
+            '受け取り後決済サービス': 'いいえ',
+            '海外発送': 'いいえ',
+            'アフィリエイト': 'はい',
+            'アフィリエイト報酬率': 1,
+            '出品者情報開示前チェック': 'いいえ'
+        }
