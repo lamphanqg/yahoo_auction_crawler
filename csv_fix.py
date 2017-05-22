f40d5b1937972b0bfac05982ef498a348bfb2372
Phan Quang Lam <lam.phan.qg@gmail.com>
2017-05-22 21:39:30 +0900

    Use UTF-8 with BOM

--- csv_fix.py
+++ csv_fix.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 with open("products.csv", "rb") as f:
-    with open("products_fixed.csv", "w", encoding='utf8') as f2:
+    with open("products_fixed.csv", "w", encoding='utf-8-sig') as f2:
         f2.write(f.read().decode().replace("\r", ""))
