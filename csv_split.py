70a1e73298b631ddeb62a11cae03ecb41c3dab69
Phan Quang Lam <lam.phan.qg@gmail.com>
2018-10-23 23:51:52 +0900

    Change spec: leave one line when remove duplicates

--- csv_split.py
+++ csv_split.py
@@ -9,28 +9,6 @@ IMAGE_COLUMNS = [8, 9, 10]
 seen = set()
 duplicates = set()
 
-def build_duplicated_title_list(titles):
-    # get duplicates from array
-    for title in titles:
-        if title not in seen:
-            seen.add(title)
-        elif title not in duplicates:
-            duplicates.add(title)
-
-
-def remove_duplicate(array):
-    duplicated_items = [item for item in array if item[TITLE_COLUMN] in duplicates]
-    for duplicated_item in duplicated_items:
-        for column in IMAGE_COLUMNS:
-            if duplicated_item[column]:
-                image_path = "images/" + duplicated_item[column]
-                if os.path.exists(image_path):
-                    os.remove(image_path)
-
-    # remove all duplicates from array
-    result = [item for item in array if item[TITLE_COLUMN] not in duplicates]
-    return result
-
 def write_to_csv(data, index):
     with open("products_fixed_" + str(index) + ".csv", "w", newline="", encoding="utf-8-sig") as f2:
         writer = csv.writer(f2)
@@ -47,24 +25,24 @@ with open("products_fixed.csv", "r", encoding='utf-8-sig', newline="") as f:
     data = []
     index = 0
 
-    for i, row in enumerate(reader):
-        data.append(row)
-    titles = [item[TITLE_COLUMN] for item in data]
-    build_duplicated_title_list(titles)
-    write_duplicate_csv(duplicates)
-
-    filtered_data = remove_duplicate(data)
-    data = None
-
     # print 300 items each csv file
-    items_to_csv = []
-    for i, row in enumerate(filtered_data):
-        items_to_csv.append(row)
-        index = i
-        if (i + 1) % MAX_LINE == 0:
-            write_to_csv([header] + items_to_csv, i + 1)
-            items_to_csv = []
+    for i, row in enumerate(reader):
+        if row[TITLE_COLUMN] not in seen:
+            seen.add(row[TITLE_COLUMN])
+            data.append(row)
+            index = i
+            if len(data) % MAX_LINE == 0:
+                write_to_csv([header] + data, i + 1)
+                data = []
+        else:
+            if row[TITLE_COLUMN] not in duplicates:
+                duplicates.add(row[TITLE_COLUMN])
+            for col in IMAGE_COLUMNS:
+                if row[col] and os.path.isfile('images/' + row[col]):
+                    os.remove('images/' + row[col])
 
     # print the rest items
-    if len(items_to_csv) >= 1:
-        write_to_csv([header] + items_to_csv, index + 1)
+    if len(data) >= 1:
+        write_to_csv([header] + data, index + 1)
+
+    write_duplicate_csv(duplicates)
